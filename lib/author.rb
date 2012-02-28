18c8d3c214ddebebb7ea897a2151d34a4bf2f965
Peter Aitken <jiggypete@Jiggys-Air.local>
2012-02-28 17:29:51 +0000

    passing builds a new key and continues the story

--- lib/author.rb
+++ lib/author.rb
@@ -5,10 +5,15 @@ class Author
 
 	def new_story_from( key_pair )
 		return "" if key_pair == ""
-		
-		possible_third_words = @inspiration[key_pair]
-		index = Kernel.rand(possible_third_words.length)
-		third_word = possible_third_words[index]
-		"#{key_pair} #{third_word}"
+
+		result = [key_pair]		
+		while @inspiration[key_pair] != nil
+			possible_third_words = @inspiration[key_pair]
+			index = Kernel.rand(possible_third_words.length)
+			third_word = possible_third_words[index]
+			result << third_word
+			key_pair = "#{key_pair.split[1]} #{third_word}"
+		end
+		result.join( ' ' )
 	end
 end
