8c5c16f1d16cc492cf10e7de481c969f6324e010
Peter Aitken <jiggypete@Jiggys-Air.local>
2012-02-28 17:32:10 +0000

    refactoring builds a new key and continues the story

--- lib/author.rb
+++ lib/author.rb
@@ -8,12 +8,16 @@ class Author
 
 		result = [key_pair]		
 		while @inspiration[key_pair] != nil
-			possible_third_words = @inspiration[key_pair]
-			index = Kernel.rand(possible_third_words.length)
-			third_word = possible_third_words[index]
+			third_word = next_word( key_pair )
 			result << third_word
 			key_pair = "#{key_pair.split[1]} #{third_word}"
 		end
 		result.join( ' ' )
+	end
+
+	def next_word( key_pair )
+		possible_third_words = @inspiration[key_pair]
+		index = Kernel.rand(possible_third_words.length)
+		possible_third_words[index]		
 	end
 end
