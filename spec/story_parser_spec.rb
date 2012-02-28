4f17bf7aaf2d1d694d21c5d472d0fec462e5b5e3
Peter Aitken <jiggypete@Jiggys-Air.local>
2012-02-28 15:39:28 +0000

    passes  builds a hash with multiple values from a story with repeating pair

--- spec/story_parser_spec.rb
+++ spec/story_parser_spec.rb
@@ -17,29 +17,29 @@ describe StoryParser do
 	end
 
 	it "builds a hash from a three word story" do
-		subject.parse( "I wish I" ).should == {'I wish' => 'I'}
+		subject.parse( "I wish I" ).should == {'I wish' => ['I']}
 	end
 
 	it "builds a hash from a four word story" do
 		subject.parse( "I wish I may" ).
-			should == {'I wish' => 'I',
-								'wish I' => 'may'}
+			should == {'I wish' => ['I'],
+								'wish I' => ['may']}
 	end
 	
 
 	it "builds a hash from a five word story" do
 		subject.parse( "I wish I may I" ).
-			should == {'I wish' => 'I',
-								 'wish I' => 'may',
-								 'I may' => 'I'}
+			should == {'I wish' => ['I'],
+								 'wish I' => ['may'],
+								 'I may' => ['I']}
 	end
 
 	it "builds a hash from a six word story" do
 		subject.parse( "I wish I may I wish" ).
-			should == {'I wish' => 'I',
-								 'wish I' => 'may',
-								 'I may' => 'I',
-									'may I' => 'wish'}
+			should == {'I wish' => ['I'],
+								 'wish I' => ['may'],
+								 'I may' => ['I'],
+									'may I' => ['wish']}
 	end
 
 	it "builds a hash with multiple values from a story with repeating pair" do
