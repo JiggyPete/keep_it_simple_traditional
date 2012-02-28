e82e7f68f356c26e4a3a96511ed4583c839edf5c
Peter Aitken <jiggypete@Jiggys-Air.local>
2012-02-28 17:16:41 +0000

    refactoring short story

--- lib/author.rb
+++ lib/author.rb
@@ -1,2 +1,9 @@
 class Author
+	def initialize(inspiration)
+		@inspiration = inspiration
+	end
+
+	def new_story_from( key_pair )
+		key_pair == "" ? "" : "#{key_pair} #{@inspiration[key_pair].first}"
+	end
 end
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -2,5 +2,14 @@ require 'spec_helper'
 
 describe Author do 
 	
-	
+	it "suffers from writers block when given an empty trigram set" do
+		subject = Author.new( {} )
+		subject.new_story_from( "" ).should == ""
+	end
+
+	it "writes a short story when trigram set has a single key with a single value" do
+		subject = Author.new( {"I wish" => ['I']} )
+		subject.new_story_from("I wish").should == "I wish I"
+	end
+
 end
