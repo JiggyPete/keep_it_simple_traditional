bb0051eaf147bc65dcc511307a37d5c235bb8cdd
Peter Aitken <jiggypete@Jiggys-Air.local>
2012-02-28 15:20:45 +0000

    failing builds a hash from a three word story

--- spec/story_parser_spec.rb
+++ spec/story_parser_spec.rb
@@ -3,15 +3,20 @@ require 'spec_helper'
 describe StoryParser do
 
 	it "builds an empty hash from an empty story" do
-		StoryParser.new.parse( "" ).should == {}
+		subject.parse( "" ).should == {}
 	end
 
 	it "builds an empty hash from a single word story" do
 		subject.parse( "I" ).should == {}
 	end
-	
+
 	it "builds an empty hash from a two word story" do
 		subject.parse( "I wish" ).should == {}
 	end
+
+	it "builds a hash from a three word story" do
+		subject.parse( "I wish I" ).should == {'I wish' => 'I'}
+	end
+
 
 end
