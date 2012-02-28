aa15319f3a3e907b7502e6e213b85670a2c9210d
Peter Aitken <jiggypete@Jiggys-Air.local>
2012-02-28 15:38:32 +0000

    passes latest test but fails others-  builds a hash with multiple values from a story with repeating pair

--- lib/story_parser.rb
+++ lib/story_parser.rb
@@ -9,7 +9,8 @@ class StoryParser
 
 		number_of_entries = (words.length - (WORDS_PER_ENTRY-1))
 		number_of_entries.times do |i|
-			result["#{words[i]} #{words[i+1]}"] = words[i+2]
+			result["#{words[i]} #{words[i+1]}"] ||= []
+			result["#{words[i]} #{words[i+1]}"] << words[i+2]
 		end
 		result
 	end
