dc22ae7eaf8387e6eb6582b0e4066516bd00b14c
Peter Aitken <jiggypete@Jiggys-Air.local>
2012-02-28 15:33:41 +0000

    passing builds a hash from a four word story

--- lib/story_parser.rb
+++ lib/story_parser.rb
@@ -1,12 +1,17 @@
 class StoryParser
 
+	WORDS_PER_ENTRY = 3
+
 	def parse(story)
 		words = story.split
-		if words.length == 3
-			{"#{words[0]} #{words[1]}" => words[2]}
-		else
-			{}
+		result = {}
+		return result if words.length < WORDS_PER_ENTRY
+
+		number_of_entries = (words.length - (WORDS_PER_ENTRY-1))
+		number_of_entries.times do |i|
+			result["#{words[i]} #{words[i+1]}"] = words[i+2]
 		end
+		result
 	end
 
 end
