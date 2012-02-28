53295f5c921dc19d973353c87f8d97ddd5cb827c
Peter Aitken <jiggypete@Jiggys-Air.local>
2012-02-28 15:23:24 +0000

    failing builds a hash from a four word story

--- spec/story_parser_spec.rb
+++ spec/story_parser_spec.rb
@@ -2,21 +2,28 @@ require 'spec_helper'
 
 describe StoryParser do
 
-	it "builds an empty hash from an empty story" do
-		subject.parse( "" ).should == {}
-	end
+	context "builds an empty hash" do
+		it "from an empty story" do
+			subject.parse( "" ).should == {}
+		end
 
-	it "builds an empty hash from a single word story" do
-		subject.parse( "I" ).should == {}
-	end
+		it "from a single word story" do
+			subject.parse( "I" ).should == {}
+		end
 
-	it "builds an empty hash from a two word story" do
-		subject.parse( "I wish" ).should == {}
+		it "from a two word story" do
+			subject.parse( "I wish" ).should == {}
+		end
 	end
 
 	it "builds a hash from a three word story" do
 		subject.parse( "I wish I" ).should == {'I wish' => 'I'}
 	end
 
+	it "builds a hash from a four word story" do
+		subject.parse( "I wish I may" ).
+			should == {'I wish' => ['I'],
+								'wish I' => ['may']}
+	end
 
 end
